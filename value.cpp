e50a38db318d562a1fbf7790c61a59ee7cdebef6
Corbin Hughes <hughes.corbin@gmail.com>
2014-01-15 15:09:47 -0600

    Changed to a single-block style variant rather than having a huge object -- values are now 8 stack bytes rather than 58

--- value.cpp
+++ value.cpp
@@ -9,36 +9,85 @@ static std::string dict_to_string(const bencode::value::dict_type& d);
 namespace bencode {
 
 value::value()
-  : type_(type::null), int_(), string_(), list_(), dict_()
+  : type_(type::null)
 { }
 
-value::value(std::int64_t v) 
-  : type_(type::integer), int_(v), string_(), list_(), dict_()
-{ }
+value::value(const int_type v) 
+  : type_(type::integer)
+{
+	construct(v);
+}
 
-value::value(const std::string& str) 
-  : type_(type::string), int_(), string_(str), list_(), dict_()
-{ }
+value::value(const string_type& str) 
+  : type_(type::string)
+{
+	construct(str);
+}
 
 value::value(const list_type& list) 
-  : type_(type::list), int_(), string_(), list_(list), dict_()
-{ }
+  : type_(type::list)
+{
+	construct(list);
+}
 
 value::value(const dict_type& dict)
-  : type_(type::dict), int_(), string_(), list_(), dict_(dict)
-{ }
+  : type_(type::dict)
+{
+	construct(dict);
+}
+
+value::value(const value& v) : type_(v.type_)
+{
+	switch(v.type_) {
+		case type::integer:
+			copy_type(v.int_value());
+			break;
+		case type::string:
+			copy_type(v.string_value());
+			break;
+		case type::list:
+			copy_type(v.list_value());
+			break;
+		case type::dict:
+			copy_type(v.dict_value());
+			break;
+		default:
+			break;
+	}
+}
+
+value::~value()
+{
+	switch (type_) {
+		case type::integer:
+			destruct_type<int_type>();
+			break;
+		case type::string:
+			destruct_type<string_type>();
+			break;
+		case type::list:
+			destruct_type<list_type>();
+			break;
+		case type::dict:
+			destruct_type<dict_type>();
+			break;
+		case type::null:
+			/* No need to do anything when empty */
+			break;
+	}
+}
 
 std::string value::encode() const
 {
 	switch (type_) {
 		case type::integer:
-			return bencode::encode(int_);
+			return bencode::encode(int_value());
 		case type::string:
-			return bencode::encode(string_);
+			return bencode::encode(string_value());
 		case type::list:
-			return bencode::encode(list_);
+			return bencode::encode(list_value());
 		case type::dict:
-			return bencode::encode(dict_);
+			return bencode::encode(dict_value());
 		case type::null:
 			throw std::runtime_error("Cannot dereference null bencode values");
 		default:
@@ -63,13 +112,13 @@ std::string bencode::value::to_string() const
 {
 	switch (type_) {
 		case type::integer:
-			return std::to_string(int_);
+			return std::to_string(int_value());
 		case type::string:
-			return string_;
+			return string_value();
 		case type::list:
-			return list_to_string(list_);
+			return list_to_string(list_value());
 		case type::dict:
-			return dict_to_string(dict_);
+			return dict_to_string(dict_value());
 		case type::null:
 			throw std::runtime_error("Cannot dereference null bencode values");
 		default:
--- value.h
+++ value.h
@@ -9,13 +9,67 @@
 #include <stdexcept>
 #include <string>
 #include <vector>
+#include <iostream>
+
+#include <type_traits>
+#include <typeinfo>
+
+namespace bencode_detail {
+	
+	template <std::size_t arg1, std::size_t ... others>
+	struct static_max;
+
+	template <std::size_t arg>
+	struct static_max<arg> {
+		static const size_t value = arg;
+	};
+
+	template <std::size_t arg1, std::size_t arg2, std::size_t ... others>
+	struct static_max<arg1, arg2, others...> {
+		static const std::size_t value = arg1 >= arg2 ? static_max<arg1, others...>::value :
+		static_max<arg2, others...>::value;
+	};
+	
+	template<typename ... Types>
+	struct union_size {
+		static const std::size_t size = static_max<sizeof(Types)...>::value;
+	};
+	
+	template<typename ... Types>
+	struct union_alignment {
+		static const std::size_t alignment = static_max<alignof(Types)...>::value;
+	};
+ 
+	template<std::size_t len, typename ... Types>
+	struct aligned_union {
+		typedef typename std::aligned_storage<static_max<len, union_size<Types...>::size>::value, 
+							union_alignment<Types...>::alignment>::type type;
+	};
+	
+	template<typename ... Types>
+	struct minimal_aligned_union {
+		typedef typename aligned_union<0, Types...>::type type;
+	};
+}
 
 namespace bencode {
 
 	class value {
-	public:
-		enum class type { null, integer, string, list, dict };
+	private:
+		template<typename T>
+		T& as()
+		{
+			return *reinterpret_cast<T*>(&storage_);
+		}
 		
+		template<typename T>
+		const T& as() const
+		{
+			return *reinterpret_cast<const T*>(&storage_);
+		}
+	public:
+		enum class type { null = 0, integer, string, list, dict };
+	
 		typedef std::int64_t int_type;
 		typedef std::string string_type;
 		typedef std::vector<value> list_type;
@@ -31,11 +85,21 @@ namespace bencode {
 		
 		explicit value(const dict_type& dict);
 		
+		value(const value& v);
+		
+		~value();
+		
 		type value_type() const { return type_; }
-		std::int64_t int_value() const { return int_; }
-		std::string string_value() const { return string_; }
-		list_type list_value() const { return list_; }
-		dict_type dict_value() const { return dict_; }
+		
+		const std::int64_t& int_value() const { return as<int_type>(); }
+		const std::string& string_value() const { return as<string_type>(); }
+		const list_type& list_value() const { return as<list_type>(); }
+		const dict_type& dict_value() const { return as<dict_type>(); }
+		
+		std::int64_t& int_value() { return as<int_type>(); }
+		std::string& string_value() { return as<string_type>(); }
+		list_type& list_value() { return as<list_type>(); }
+		dict_type& dict_value() { return as<dict_type>(); }
 		
 		std::string encode() const;
 		
@@ -45,14 +109,40 @@ namespace bencode {
 		
 		std::string to_string() const;
 		
+		value& operator=(value v)
+		{
+			swap(*this, v);
+			return *this;
+		}
+		
 	private:
 		type type_;
-		int_type int_;
-		string_type string_;
-		list_type list_;
-		dict_type dict_;
+		
+		bencode_detail::minimal_aligned_union<int_type, string_type, 
+											  list_type, dict_type>::type storage_;
+		
+		template<typename T>
+		void construct(const T& val)
+		{
+			new (&storage_) T(val);
+		}
+		
+		template<typename T>
+		void destruct_type() { reinterpret_cast<T*>(&storage_)->~T(); }
+		
+		template<typename T>
+		void copy_type(const T& val)
+		{ construct(val); }
+		
+		friend void swap(value& first, value& second)
+		{
+			using std::swap;
+			swap(first.type_, second.type_); 
+			swap(first.storage_, second.storage_);
+		}
+		
 	};
-
+	
 }
 
 #endif
